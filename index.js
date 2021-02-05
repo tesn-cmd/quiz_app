1a3bfd10e9ba9e6020f13f8e135832c4f36545a8
tesn-cmd <68950931+tesn-cmd@users.noreply.github.com>
2021-02-05 13:22:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const passentage = document.getElementById('passentage');
 //feching data from json
 async function getQuestions() {
   try {
-    let response = await fetch('./questions.json');
+    let response = await fetch('https://github.com/tesn-cmd/quiz_app/blob/main/questions.json');
     let data = await response.json();
     let allQuestion = data.questions;
     questionList(allQuestion)
